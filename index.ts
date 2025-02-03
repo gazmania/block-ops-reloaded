f3f6b654937cfe6e9c4415fa53e0d9b2bd24966a
gazmania <gary.poore@gmail.com>
2025-02-03 23:26:33 +1300

    WIP - world hopping

--- index.ts
+++ index.ts
@@ -4,72 +4,115 @@ import {
   PlayerEntity,
   Entity,
   WorldMap,
-  RigidBodyType
+  RigidBodyType,
+  Collider,
+  BlockType
 } from 'hytopia';
 
-import MyEntityController from './MyEntityController';
 
 import mapData from './assets/maps/boilerplate.json';
+import { GunWorld } from './sessions/world';
 
 startServer(world => {
   // Включаем отладку физики, чтобы видеть коллайдеры
-    //world.simulation.enableDebugRaycasting(true);
-    //world.simulation.enableDebugRendering(true);
+  //world.simulation.enableDebugRaycasting(true);
+  //world.simulation.enableDebugRendering(true);
 
   // Загружаем базовую карту
   world.loadMap(mapData);
 
-  // Создаем и спавним машину с динамической физикой
-  const carEntity = new Entity({
-    modelUri: 'models/environment/car.gltf',
-    modelScale: 0.7,
-    rigidBodyOptions: {
-      type: 'dynamic' as RigidBodyType, // Динамическое тело, будет реагировать на физику
-      position: { x: 10, y: 5, z: 10 },
-      rotation: { x: 0, y: 0, z: 0, w: 1 }
-    }
-  });
-  carEntity.spawn(world, { x: 10, y: 5, z: 10 }); // Спавним выше земли, чтобы увидеть падение
+  // // Создаем и спавним машину с динамической физикой
+  // const carEntity = new Entity({
+  //   modelUri: 'models/environment/car.gltf',
+  //   modelScale: 0.7,
+  //   rigidBodyOptions: {
+  //     type: 'dynamic' as RigidBodyType, // Динамическое тело, будет реагировать на физику
+  //     position: { x: 10, y: 5, z: 10 },
+  //     rotation: { x: 0, y: 0, z: 0, w: 1 }
+  //   }
+  // });
+  // carEntity.spawn(world, { x: 10, y: 5, z: 10 }); // Спавним выше земли, чтобы увидеть падение
 
-  // Создаем и спавним здание
-  const buildingEntity = new Entity({
-    modelUri: 'models/environment/build1.gltf',
-    modelScale: 0.5,
+  // // Создаем и спавним здание
+  // const buildingEntity = new Entity({
+  //   modelUri: 'models/environment/build1.gltf',
+  //   modelScale: 0.5,
+  //   rigidBodyOptions: {
+  //     type: 'fixed' as RigidBodyType, // Фиксированное тело, не будет двигаться
+  //     position: { x: 0, y: 0, z: 0 },
+  //     rotation: { x: 0, y: 0, z: 0, w: 1 }
+  //   }
+  // });
+  // buildingEntity.spawn(world, { x: -10, y: 2, z: -10 });
+
+
+
+  const worldRegistry: Record<string, GunWorld> = {
+    "one-on-one": new GunWorld({
+      id: 2,
+      name: "One on One Action",
+      skyboxUri: "./assets/skyboxes/partly-cloudy",
+      minPlayerCount: 2,
+      maxPlayerCount: 2,
+      maxWaitingTime: 10 * 1000,
+    })
+  }
+
+  Object.values(worldRegistry).forEach(world => {
+    console.log(`Starting world ${world.name}`);
+    world.start();
+  });
+
+
+  const oneOnOneGatewayEntity = new Entity({
+    name: "one-on-one",
+    blockHalfExtents: { x: 0.5, y: 0.5, z: 0.5 },
+    blockTextureUri: "blocks/sand.png",
     rigidBodyOptions: {
-      type: 'fixed' as RigidBodyType, // Фиксированное тело, не будет двигаться
-      position: { x: 0, y: 0, z: 0 },
-      rotation: { x: 0, y: 0, z: 0, w: 1 }
+      type: RigidBodyType.FIXED,
+      colliders: [
+        {
+          ...Collider.optionsFromBlockHalfExtents({ x: 0.5, y: 0.5, z: 0.5 }),
+          onCollision: (other: BlockType | Entity, started: boolean) => {
+            if (!started) return;
+            if (!(other instanceof PlayerEntity)) return;
+
+            const playerEntity = other as PlayerEntity;
+
+            const gameWorld = worldRegistry["one-on-one"];
+            if (gameWorld.playerCount >= gameWorld.maxPlayerCount) {
+              console.log(`[Player ${playerEntity.name}] can't get into game '${gameWorld.name}' as it's full; player count ${gameWorld.playerCount}`);
+            }
+
+            console.log(`[Player ${playerEntity.name}] Is being sent to game '${gameWorld.name}' with ${gameWorld.playerCount} other players`)
+            gameWorld.join(playerEntity.player);
+
+          }
+        }
+      ],
     }
   });
-  buildingEntity.spawn(world, { x: -10, y: 2, z: -10 });
+  oneOnOneGatewayEntity.spawn(world, { x: 10, y: 1.5, z: 10 });
 
   world.onPlayerJoin = player => {
     const playerEntity = new PlayerEntity({
       player,
       name: 'Player',
       modelUri: 'models/players/PlayerModel.gltf',
-      modelLoopedAnimations: [ 'idle' ],
-      modelScale: 0.5,
-      controller: new MyEntityController(),
+      modelLoopedAnimations: ['idle'],
+      modelScale: 0.5
     });
 
     player.camera.setMode(PlayerCameraMode.FIRST_PERSON);
     player.camera.setOffset({ x: 0, y: 0.4, z: 0 });
-    player.camera.setModelHiddenNodes([ 'head', 'neck' ]);
+    player.camera.setModelHiddenNodes(['head', 'neck']);
     player.camera.setForwardOffset(0.3);
-    
-    // Спавним игрока на безопасном расстоянии от объектов
-    if (world) {
-        playerEntity.spawn(world, { x: 0, y: 2, z: 0 });
-        console.log('Spawned player entity!');
-    } else {
-        console.error('World is undefined!');
-    }
-    
-    player.ui.load('ui/index.html');
+
+    playerEntity.spawn(world, { x: 0, y: 4, z: 0 });
   };
-  
+
   world.onPlayerLeave = player => {
+    console.log("Player Leavng WOerld")
     world.entityManager.getPlayerEntitiesByPlayer(player).forEach(entity => entity.despawn());
   };
 });
---
+++ sessions/world.ts
@@ -0,0 +1,107 @@
+import { Player, PlayerCameraMode, PlayerEntity, World, WorldOptions } from "hytopia";
+import MyEntityController from "../MyEntityController";
+import mapData from '../assets/maps/boilerplate.json';
+
+export interface GunWorldOptions extends WorldOptions {
+    minPlayerCount: number;
+    maxPlayerCount: number;
+    maxWaitingTime: number;
+}
+
+enum GunWorldPlayState {
+    WAITING, ACTIVE, FINISHED
+}
+
+export interface GunWorldState {
+    startTime: number;
+    players: Player[];
+    playState: GunWorldPlayState;
+}
+
+export class GunWorld extends World {
+    private _minPlayerCount: number;
+    private _maxPlayerCount: number;
+    private _maxWaitingTime: number;
+
+    private _worldState: GunWorldState;
+
+    public get maxPlayerCount(): number { return this._maxPlayerCount; }
+    public get playerCount(): number { return this._worldState.players.length; }
+
+    constructor(options: GunWorldOptions) {
+        super(options)
+
+        this._minPlayerCount = options.minPlayerCount;
+        this._maxPlayerCount = options.maxPlayerCount;
+        this._maxWaitingTime = options.maxWaitingTime;
+
+        this._worldState = {
+            startTime: Date.now(),
+            players: [],
+            playState: GunWorldPlayState.WAITING
+        };
+
+        this.loadMap(mapData);
+
+        // const waitingRoomInterval = setInterval(() => {
+        //     if (this._worldState.playState == GunWorldPlayState.WAITING &&
+        //         this._worldState.startTime - Date.now() < this._maxWaitingTime &&
+        //         this._worldState.players.length < this._minPlayerCount) {
+        //         return;
+        //     }
+    
+        //     // if any of the above guards are false we'll get here
+        //     // start the game
+        //     this._worldState.playState = GunWorldPlayState.ACTIVE;
+    
+        //     clearInterval(waitingRoomInterval);
+    
+        // }, 1000);
+    }
+
+    public join(player: Player) {
+        if (!player) return false;
+        if (this.playerCount >= this.maxPlayerCount) return false;
+
+        player.joinWorld(this);
+
+        return true;
+    }
+
+    /**
+     * A function that is called when a player joins the world.
+     */
+    onPlayerJoin = (player: Player) => {
+        const playerEntity = new PlayerEntity({
+            player,
+            name: 'Player',
+            modelUri: 'models/players/PlayerModel.gltf',
+            modelLoopedAnimations: ['idle'],
+            modelScale: 0.5,
+            // controller: new MyEntityController(),
+        });
+
+        // player.camera.setMode(PlayerCameraMode.FIRST_PERSON);
+        // player.camera.setOffset({ x: 0, y: 0.4, z: 0 });
+        // player.camera.setModelHiddenNodes(['head', 'neck']);
+        // player.camera.setForwardOffset(0.3);
+
+        // Спавним игрока на безопасном расстоянии от объектов
+
+        playerEntity.spawn(this, { x: Math.random() * 10 - 5, y: 4, z: Math.random() * 10 - 5 });
+        console.log('Spawned player entity!');
+
+
+        // player.ui.load('ui/index.html');
+
+        this._worldState.players.push(player);
+    }
+
+    /**
+     * A function that is called when a player leaves the world.
+     */
+    onPlayerLeave = (player: Player) => {
+        this._worldState.players = this._worldState.players.filter(p => p !== player);
+        this.entityManager.getPlayerEntitiesByPlayer(player).forEach(entity => entity.despawn());
+    }
+}
